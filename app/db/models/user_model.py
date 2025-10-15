7b0e865232cefca9838b07149c4b3a47fbf70b89
himanshu1-sharma <sharmagi1998@gmail.com>
2025-10-15 14:29:56 +0530

    Add timestamps to user model and schema for created_at and updated_at fields

--- app/db/models/user_model.py
+++ app/db/models/user_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String # type: ignore
+from sqlalchemy import Column, Integer, String, DateTime, func # type: ignore
 from app.db.session import Base
 import uuid
 from sqlalchemy.dialects.postgresql import UUID
@@ -9,3 +9,5 @@ class User(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     name = Column(String(100), nullable=False)
     email = Column(String(120), unique=True, index=True, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,15 +1,19 @@
 from pydantic import BaseModel, EmailStr # type: ignore
 import uuid
+from datetime import datetime
 
 class UserBase(BaseModel):
     name: str
     email: EmailStr
+    
 
 class UserCreate(UserBase):
     pass
 
 class UserOut(UserBase):
     id: uuid.UUID
+    created_at: datetime
+    updated_at: datetime
 
     class Config:
         orm_mode = True
